ed4e8d133d9ebe3751a5520679e0978db475a91e
Anspiriti <127615447+anspiriti@users.noreply.github.com>
2023-03-23 17:03:01 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/publicdomain/zero/1.0/
 
+window.addEventListener("load", doStartup, false);
+
+function doStartup(event) {
+  document.fullscreenElement = document.fullscreenElement || document.mozFullscreenElement
+    || document.msFullscreenElement || document.webkitFullscreenDocument;
+  document.exitFullscreen = document.exitFullscreen || document.mozExitFullscreen
+    || document.msExitFullscreen || document.webkitExitFullscreen;
+
+  document.addEventListener("click", handleKeypress, false);
+}
+
+function handleKeypress(event) {
+  if (event.keyCode === 13) {
+    toggleFullscreen();
+  }
+}
+
 function hideScreenButtonChoiceFullScreen() {
   document.getElementById('full-screen-btn').style.visibility = 'hidden';
   document.getElementById('normal-screen-btn').style.visibility = 'hidden';
